59cf69dff5f143abaf12742b682cafd777d01875
Angelo Keirsebilck <angelokeirsebilck@Mac-Pro-van-Angelo.local>
2022-05-02 06:20:00 +0200

    Basic setup ddev vite

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+import '@/css/app.css'
+import { createApp } from 'vue';
+
+import ConfettiParty from '@/vue/ConfettiParty.vue'
+
+const main = async () => {
+    // Create our vue instance
+    const app = createApp(ConfettiParty);
+    // Mount the app
+    app.mount('.confetti');
+};
+
+main().then(() => {
+    console.log("qzdqqzdzd test ");
+});
+
+
+if (import.meta.hot) {
+    import.meta.hot.accept(() => {
+        console.log("HMR")
+    });
+}
---
+++ src/vue/ConfettiParty.vue
@@ -0,0 +1,22 @@
+<template>
+  <div />
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import Confetti from 'vue-confetti/src/confetti.js';
+
+const config = {
+  defaultType: 'heart',
+  defaultSize: 50,
+  defaultColors: ['Pink','Green','Red','Yellow'],
+};
+const confetti = new Confetti();
+
+onMounted(() => {
+  confetti.start(config);
+  setTimeout(() => {
+    confetti.stop();
+  }, 5000);
+});
+</script>
