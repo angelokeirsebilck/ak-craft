96a409ed1eeffe9f3647f7d8afd6ef6ad320fa59
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-06-26 20:31:38 +0200

    Fix underline svg

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,49 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-import legacy from '@vitejs/plugin-legacy';
-import ViteRestart from 'vite-plugin-restart';
-import viteCompression from 'vite-plugin-compression';
-import manifestSRI from 'vite-plugin-manifest-sri';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import ViteRestart from 'vite-plugin-restart'
+import viteCompression from 'vite-plugin-compression'
+import manifestSRI from 'vite-plugin-manifest-sri'
 // import {visualizer} from 'rollup-plugin-visualizer';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import critical from 'rollup-plugin-critical';
-import copy from 'rollup-plugin-copy';
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import critical from 'rollup-plugin-critical'
+import copy from 'rollup-plugin-copy'
 // import {ViteFaviconsPlugin} from 'vite-plugin-favicon2';
-import * as path from 'path';
+import * as path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command}) => ({
+export default defineConfig(({ command }) => ({
     base: command === 'serve' ? '' : '/dist/',
     build: {
         emptyOutDir: true,
         manifest: true,
         commonjsOptions: {
-            transformMixedEsModules: true,
+            transformMixedEsModules: true
         },
         outDir: 'web/dist',
         rollupOptions: {
             input: {
-                app: './src/js/app.js',
+                app: './src/js/app.js'
             },
             output: {
                 sourcemap: true
-            },
+            }
         }
     },
     plugins: [
         critical({
             criticalUrl: 'http://localhost',
             criticalBase: 'web/dist/criticalcss/',
-            criticalPages: [
-                {uri: '/', template: 'index'},
-            ],
-            criticalConfig: {}
+            criticalPages: [{ uri: '/', template: 'index' }],
+            criticalConfig: {
+                ignore: ['.underline']
+            }
         }),
         // legacy({
         //     targets: ['defaults', 'not IE 11']
         // }),
         nodeResolve({
-            moduleDirectories: [
-                path.resolve('./node_modules'),
-            ],
+            moduleDirectories: [path.resolve('./node_modules')]
         }),
         // ViteFaviconsPlugin({
         //     logo: "src/img/favicon.png",
@@ -53,9 +51,7 @@ export default defineConfig(({command}) => ({
         //     outputPath: 'favicons',
         // }),
         ViteRestart({
-            reload: [
-                'templates/**/*',
-            ],
+            reload: ['templates/**/*']
         }),
         vue({
             customElement: true
@@ -63,7 +59,7 @@ export default defineConfig(({command}) => ({
         viteCompression({
             filter: /\.(js|mjs|json|css|map)$/i
         }),
-        manifestSRI(),
+        manifestSRI()
         // visualizer({
         //     filename: 'web/dist/assets/stats.html',
         //     template: 'treemap',
@@ -80,12 +76,12 @@ export default defineConfig(({command}) => ({
             '@': path.resolve(__dirname, './src'),
             vue: 'vue/dist/vue.esm-bundler'
         },
-        preserveSymlinks: true,
+        preserveSymlinks: true
     },
     server: {
         origin: 'http://localhost:3000',
         host: '0.0.0.0',
         port: 3000,
-        strictPort: true,
-    },
-}));
+        strictPort: true
+    }
+}))
