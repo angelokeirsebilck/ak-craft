e4551ddc3da381118b98666ebe234f5aeb370a41
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-06-27 06:14:44 +0200

    Fix underline position + fix cta grid

--- web/dist/assets/app.34bdab47.js
+++ web/dist/assets/app.34bdab47.js
@@ -1,2 +1,2 @@
 const o=async()=>{},r=()=>{document.querySelectorAll("h2 strong").forEach(t=>{t.classList.add("bg-underline","bg-no-repeat","bg-bottom","font-medium")})};o().then(()=>{window.onload=e=>{r()}});
-//# sourceMappingURL=app.1d28db11.js.map
+//# sourceMappingURL=app.34bdab47.js.map
