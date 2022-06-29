cb3db16f27c5f48e7ebbb63ec1156e636b184d48
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-06-29 20:12:01 +0200

    Add video to text columns block

--- web/dist/assets/app.b3094192.js
+++ web/dist/assets/app.b3094192.js
@@ -1,2 +1,2 @@
 const o=async()=>{},r=()=>{document.querySelectorAll("h2 strong").forEach(t=>{t.classList.add("bg-underline","bg-no-repeat","bg-bottom","font-medium")})};o().then(()=>{window.onload=e=>{r()}});
-//# sourceMappingURL=app.34bdab47.js.map
+//# sourceMappingURL=app.b3094192.js.map
